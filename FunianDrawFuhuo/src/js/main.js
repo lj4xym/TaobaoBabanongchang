6b0b032fab7ed2d8b3303d857897bd1bbb9787b8
lj4xym <liujian4xym@gmail.com>
2021-02-03 17:09:29 +0800

    1.0.0 新建多项目。

---
+++ FunianDrawFuhuo/src/js/main.js
@@ -0,0 +1,58 @@
+/**
+ * 常用JS变量:
+ * agentEvent = 代理模式下自动点击模块
+ * acEvent= 无障碍模式下自动点击模块
+ * device = 设备信息模块
+ * file = 文件处理模块
+ * http = HTTP网络请求模块
+ * shell = shell命令模块
+ * thread= 多线程模块
+ * image = 图色查找模块
+ * utils= 工具类模块
+ * global = 全局快捷方式模块
+ * 常用java变量：
+ *  context : Android的Context对象
+ *  javaLoader : java的类加载器对象
+ * 导入Java类或者包：
+ *  importClass(类名) = 导入java类
+ *      例如: importClass(java.io.File) 导入java的 File 类
+ *  importPackage(包名) =导入java包名下的所有类
+ *      例如: importPackage(java.util) 导入java.util下的类
+ *
+ */
+
+function main() {
+    //开始再这里编写代码了！！
+    toast("Hello World");
+    var name = readConfigString("name");
+    logd("姓名: " + name);
+    logd("年龄: " + readConfigString("age"));
+    logd("听音乐: " + readConfigString("music"));
+    logd("是不是一年级: " + readConfigString("one"));
+    logd("备注: " + readConfigString("mark"));
+    logd("jobTaskTag..." + readConfigString("jobTaskTag"));
+    //如果自动化服务正常
+    if (!autoServiceStart(3)) {
+        logd("自动化服务启动失败，无法执行脚本")
+        exit();
+        return;
+    }
+    logd("开始执行脚本...")
+    home();
+}
+
+function autoServiceStart(time) {
+    for (var i = 0; i < time; i++) {
+        if (isServiceOk()) {
+            return true;
+        }
+        var started = startEnv();
+        logd("第" + (i + 1) + "次启动服务结果: " + started);
+        if (isServiceOk()) {
+            return true;
+        }
+    }
+    return isServiceOk();
+}
+
+main();
---
+++ FunianDrawFuhuo/src/layout/ui.js
@@ -0,0 +1,12 @@
+function main() {
+    // 参数设置 = main.html
+    // 使用说明 = intr.html
+    // 定时任务 = timer.html
+    // 其他 = other.html
+    ui.layout("参数设置", "main.html");
+    ui.layout("使用说明", "intr.html");
+    ui.layout("定时任务", "timer.html");
+    ui.layout("其他", "other.html");
+}
+
+main();
